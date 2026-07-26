70a09f69f8ec2a82f0f0e79d656a1ebd8d7fbaf7
Davi <davi.giacomel@aluno.senair.br>
2026-07-26 13:46:56 -0300

    Corrige acesso ao index

--- Backend/server.js
+++ Backend/server.js
@@ -18,6 +18,10 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "..")));
 
+app.get("/index.html", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "index.html"));
+});
+
 // ========================================
 // CONFIGURAÇÃO DO UPLOAD DE MANUAIS
 // ========================================
